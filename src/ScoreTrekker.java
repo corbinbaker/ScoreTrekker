a309758d5542c5ae67efa93bb9aef60fd5258762
Corbin Baker <corbinbaker@mymail.mines.edu>
2016-09-27 15:15:31 -0600

    Initial commit to repository

---
+++ src/ScoreTrekker.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ScoreTrekker implements Comparable<Student> {
+
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
+	
+	private ArrayList<Student> students = new ArrayList<Student>();
+
+	@Override
+	public int compareTo(Student arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private void loadDataFromFile()
+	{
+		
+	}
+	
+	private void printInOrder()
+	{
+		
+	}
+	
+	private void processFiles()
+	{
+		
+	}
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
