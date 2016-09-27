8262da2d4d61ba4c8be3f97a3d205f8b65fdd350
Corbin Baker <corbinbaker@mymail.mines.edu>
2016-09-27 16:08:40 -0600

    Finished loadDataFromFile, fixed printInOrder; FINISHED PART I: READING FROM A FILE

--- src/ScoreTrekker.java
+++ src/ScoreTrekker.java
@@ -1,35 +1,46 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ScoreTrekker {
 
-	private String file = "score.txt";
+	private String file = "scores.txt";
 	//private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
 	
 	private ArrayList<Student> students = new ArrayList<Student>();
 
 	
-	private void loadDataFromFile(String fileName)
+	private void loadDataFromFile(String fileName) throws FileNotFoundException
 	{
+		FileReader reader = new FileReader(fileName);
+		Scanner in = new Scanner(reader);
+		
+		while(in.hasNextLine())
+		{
+			String name = in.nextLine();
+			int score = Integer.parseInt(in.nextLine());
+			
+			students.add(new Student(name, score));
+		}
 		
 	}
 	
 	private void printInOrder()
 	{
-		Student[] sarray = (Student[]) students.toArray();
-		for(Student student : sarray)
+		for(Student student : students)
 		{
 			System.out.println(student);
 		}
 	}
 	
-	private void processFiles()
+	private void processFiles() throws FileNotFoundException
 	{
 		loadDataFromFile(file);
 		printInOrder();
 	}
 	
-	public static void main(String args[])
+	public static void main(String args[]) throws FileNotFoundException
 	{
 		ScoreTrekker scoretrekker = new ScoreTrekker();
 		scoretrekker.processFiles();
